90aa2a88832c8085d2ed713dae2d5453b5464895
burundtriogeeks <burund@triogeeks.com>
2023-02-20 14:38:27 +0200

    Done

--- src/index.php
+++ src/index.php
@@ -1,3 +1,20 @@
 <?php
+    //phpinfo();
+    //exit;
+    require './vendor/autoload.php';
 
-    echo "URARA";
+
+    $client = new MongoDB\Client("mongodb://admin:admin@mongodb:27017");
+    $collection = $client->demo->beers;
+
+    $result = $collection->insertOne( [ 'name' => 'Hinterland', 'brewery' => 'BrewDog' ] );
+
+    echo "Inserted with Object ID '{$result->getInsertedId()}'\n";
+
+    $client = Elastic\Elasticsearch\ClientBuilder::create()
+    ->setHosts(['elasticsearch:9200'])
+    ->build();
+    $response = $client->info();
+
+    echo "Elasticsearch version:" . $response['version']['number'] . "\n";
+    echo "End";
